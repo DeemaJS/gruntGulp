1650a5190c136b854fa4914daff410e78c45c793
DeemaJS <deeema@outlook.com>
2015-10-21 16:02:40 +0300

    Second task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,3 +3,10 @@ var grunt = require('grunt');
 grunt.registerTask('default', 'description', function() {
 	console.log('First task');
 });
+
+grunt.registerTask('hello', 'say hello', function(name) {
+	if(!name || !name.length)
+		grunt.warn('Wher is a NAME?');
+
+	console.log('Good day ' + name);
+});
